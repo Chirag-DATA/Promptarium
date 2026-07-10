745c90326f7ec05a54d06d8864a7559db12a77c1
Chirag-DATA <chirag.personal.2004@gmail.com>
2026-07-10 16:25:28 +0530

    Hide Login button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -112,12 +112,12 @@ const Navbar = () => {
               {link.label}
             </NavLink>
           ))}
-          <button
+          {/* <button
             type="button"
             className="rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white text-left w-fit"
           >
             Login
-          </button>
+          </button> */}
         </div>
       )}
     </nav>
